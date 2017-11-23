ead32d6b5112e51bc2417e833800d738cecf7b43
Toai Nguyen Van <toainv@larion.com>
2017-11-23 10:05:22 +0700

    remove assets

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,6 @@ module FoodDemo
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.assets.enabled = true
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
